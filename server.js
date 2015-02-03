8e4af221b5a1b372c8fc1e5a92969edb3032b665
Jingo88 <jcory.ng@gmail.com>
2015-02-03 10:58:59 -0500

    included npm sessions to prevent users from jumping to secret page

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ var request = require('request');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var session = require('express-session');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //we do line 12 because we do not want to push the password up to Github
 //the (./) means in the directory I am in
 var secret = require('./secret.json');
+
+app.use(session({
+  secret: secret.password,
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 app.get('/', function(req,res){
@@ -27,9 +36,9 @@ app.get('/', function(req,res){
 //now the if statement is (secret.password) because we are looking into the file to grab the password
 //inside the hash.
 
-app.get('/login', function(req,res){
-	console.log(req.query)
-	if (req.query.password === secret.password){
+app.post('/session', function(req,res){
+	if (req.body.password === secret.password){
+		req.session.valid_user = true;
 		res.redirect('/secret_page');
 	} else {
 		res.redirect('/')
@@ -37,11 +46,19 @@ app.get('/login', function(req,res){
 });
 
 app.get('/secret_page', function(req,res){
-	res.send('Hello! <a href="/secret_page2"> Secret Page 2 </a>');
+	if (req.session.valid_user === true){
+		res.send('Hello! <a href="/secret_page2"> Secret Page 2 </a>');
+	} else {
+		res.redirect('/');
+	} 
 });
 
 app.get('/secret_page2', function(req,res){
-	res.send("Hello Again! <a href='/secret_page'> Back to Secret Page 1 </a>")
+	if (req.session.valid_user === true) {
+		res.send("Hello Again! <a href='/secret_page'> Back to Secret Page 1 </a>")
+	} else {
+		res.redirect('/');
+	}
 })
 
 app.listen(3000);
